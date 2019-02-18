aba2cdef8ffa421d4f50bd1f97817638c157c277
giovannicontestabile <gio.contestabile@gmail.com>
2019-02-18 17:21:20 +0100

    quasi

--- src/main/java/it/Controller/Hello.java
+++ src/main/java/it/Controller/Hello.java
@@ -1,19 +1,27 @@
 package it.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 @RestController
 public class Hello {
 
-    @GetMapping("/hello")
-    public Collection<String> sayHello() {
+    @RequestMapping(value="/hello/{id}/{name}",method= RequestMethod.GET)
+    public String sayHello(@PathVariable("id") Integer id,@PathVariable("name") String name){
+        return "id:"+id+" name:"+name;
+    }
+}
+
+
+
+    /*public Collection<String> sayHello() {
         return IntStream.range(0, 10)
                 .mapToObj(i -> "Hello number " + i)
                 .collect(Collectors.toList());
     }
-}
+}*/
---
+++ src/main/java/it/View/out.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html >
+<head>
+    <title>Title</title>
+</head>
+<body>
+<div>Hello ;) .</br> text="${message}"</div>
+</body>
+</html>
