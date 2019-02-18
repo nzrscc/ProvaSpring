022d25a7bcc50996cb254862c1b94a3f612c8b4f
giovannicontestabile <gio.contestabile@gmail.com>
2019-02-18 18:05:52 +0100

    daje

--- src/main/java/controller/SpringBootWebApplication.java
+++ src/main/java/controller/SpringBootWebApplication.java
@@ -2,16 +2,14 @@ package controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.*;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-@SpringBootApplication
-public class SpringBootWebApplication extends SpringBootServletInitializer {
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.*;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringBootWebApplication.class);
-	}
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringBootWebApplication.class, args);
--- src/main/webapp/WEB-INF/view/index.jsp
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -6,7 +6,16 @@
 		<div>
 			<h1>Spring Boot JSP Example</h1>
 			<h2>Hello ${message}</h2>
-			
+
+			<form action="next" method="get">
+				<h1>Inerisci nome</h1>
+			<br>
+				<input type="text" value="name">
+				<br>
+				<input type="submit" value="Clicca qui!">
+			<br>
+			</form>
+
 			Click on this <strong><a href="next">link</a></strong> to visit another page.
 		</div>
 	</div>
